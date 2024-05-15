90b3bab5cac77bdbd2a9f19cce2bc8d133127431
Muarif Shaikh <muarif.s@nimapinfotech.com>
2024-05-15 19:05:47 +0530

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            divide(5, 0);
+           
+        }
+        public static void divide(int a, int b)
+        {
+            try { 
+              var result = a / b;
+        }
+            catch(DivideByZeroException ex){ 
+                Console.WriteLine("You Can not divide by zero");
+            }
+            catch(ArithmeticException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ UserDefinedException/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefinedException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                divide(5, 0);
+            }
+            catch (SelfDefineException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+        }
+        public static void divide(int a, int b)
+        {
+            try
+            {
+                var result = a / b;
+            }
+            catch (Exception e)
+            {
+                throw new SelfDefineException("You Can not Divided By Zero please Check", e);
+            }
+        }
+    }
+}
